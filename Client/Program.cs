485802ce355f08a30f3de04aa3008fb1d026284c
lidiof <lidiofelipe@gmail.com>
2020-01-04 23:51:03 -0300

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,22 @@
+﻿using Minhalib2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("teste Scrape!! \n");
+
+            Scrape myscrape = new Scrape();
+            string v = myscrape.ScrapeWebpage("https://pt.wikipedia.org/wiki/C_Sharp");
+            Console.WriteLine(v);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Minhalib2/Scrape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minhalib2
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+
+        }
+        public string ScrapeWebpage(string url, string file)
+        {
+
+            string reply = GetWebpage(url);
+
+            File.WriteAllText(file, reply);
+            return reply;
+        }
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+
+        }
+    }
+}
